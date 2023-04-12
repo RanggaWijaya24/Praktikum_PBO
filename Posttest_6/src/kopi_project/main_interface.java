6fc480740b21025f0437ffdd58ae7403f61364aa
RanggaWijaya24 <113325745+RanggaWijaya24@users.noreply.github.com>
2023-04-12 15:52:23 +0800

    Add files via upload

---
+++ Posttest_6/src/kopi_project/main_interface.java
@@ -0,0 +1,8 @@
+package kopi_project;
+import java.io.*;
+public interface main_interface {
+    // void nambah_kopi() throws NumberFormatException, IOException;
+    // void delete_Data() throws NumberFormatException, IOException;
+    void lht();
+    public void setHarga(int harga);
+}
